55416a441de200775d2fdb2eed8532537ddacf2e
Chirag-Coush <studio_@live.com>
2026-05-18 12:45:04 +0300

    Fix deployed gallery image URLs

--- src/scripts/life-at-rossholme.js
+++ src/scripts/life-at-rossholme.js
@@ -4,6 +4,13 @@ import "photoswipe/style.css";
 
 initPage();
 
+document.querySelectorAll('[data-gallery="faces"] a').forEach((link) => {
+  const image = link.querySelector("img");
+  if (image?.currentSrc || image?.src) {
+    link.href = image.currentSrc || image.src;
+  }
+});
+
 const facesGallery = new PhotoSwipeLightbox({
   gallery: '[data-gallery="faces"]',
   children: "a",
--- src/scripts/our-story.js
+++ src/scripts/our-story.js
@@ -4,6 +4,13 @@ import "photoswipe/style.css";
 
 initPage();
 
+document.querySelectorAll('[data-gallery="history"] a').forEach((link) => {
+  const image = link.querySelector("img");
+  if (image?.currentSrc || image?.src) {
+    link.href = image.currentSrc || image.src;
+  }
+});
+
 const historyGallery = new PhotoSwipeLightbox({
   gallery: '[data-gallery="history"]',
   children: "a",
