f1c8b14b628e93827116146d7e486a5530c5ce58
Chirag-Coush <studio_@live.com>
2026-05-29 22:05:25 +0300

    updated the gallery in get involved page and replaced few images and pdf with optimised version

--- src/scripts/get-involved.js
+++ src/scripts/get-involved.js
@@ -4,6 +4,13 @@ import "photoswipe/style.css";
 
 initPage();
 
+document.querySelectorAll('[data-gallery="ramble-history"] a').forEach((link) => {
+  const image = link.querySelector("img");
+  if (image?.currentSrc || image?.src) {
+    link.href = image.currentSrc || image.src;
+  }
+});
+
 const rambleHistoryGallery = new PhotoSwipeLightbox({
   gallery: '[data-gallery="ramble-history"]',
   children: "a",
